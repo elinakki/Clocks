330ec6640a887a1bbdc52886ec1b49d2306ab5b2
elinakki <n1rael00@students.oamk.fi>
2022-09-19 12:42:09 +0300

    Tehty valmiiksi, lisätty muistiinpanoja

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+      // tänne koodia joka näkyy kun sivu avataan/ajastin käynnistetään
+      const id = setInterval(() => { // anonyymi funktio, ei nimeä + aaltosulkeet
+        setTime(new Date())
+      },1000) // päivitetään tuhannen millisekunnin välein
+  
+    return () => {
+      // tänne koodia joka näkyy kun sivu/ajastin suljetaan
+      // useEffectissä returnia ei usein tarvita
+      clearInterval(id)
+    }
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '30px'}}>
+
+      {time.toLocaleTimeString()}
+
     </div>
   );
 }
